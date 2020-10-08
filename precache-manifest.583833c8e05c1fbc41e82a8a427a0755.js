50594ea44b8f27b6403bfe0e42cc1f252050c3ae
abdo abbas <abdabbas456@gmail.com>
2020-10-08 20:11:03 +0300

    Updates

---
+++ precache-manifest.583833c8e05c1fbc41e82a8a427a0755.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd9fbf7dbee2084161629011277807d",
+    "url": "/star/index.html"
+  },
+  {
+    "revision": "6cf79edc36ece7757dd6",
+    "url": "/star/static/css/main.b07e137b.chunk.css"
+  },
+  {
+    "revision": "a60fc69a2ab9d9d88488",
+    "url": "/star/static/js/2.f2e35bae.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/star/static/js/2.f2e35bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf79edc36ece7757dd6",
+    "url": "/star/static/js/main.043e929b.chunk.js"
+  },
+  {
+    "revision": "fe356c4cc5a7580239d8",
+    "url": "/star/static/js/runtime-main.dce63f6c.js"
+  },
+  {
+    "revision": "32b847c5e4afdda668f1d04e72688498",
+    "url": "/star/static/media/Mask Group 3.32b847c5.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/star/static/media/bars.b3f49afd.svg"
+  },
+  {
+    "revision": "9d7f51056f54c2b79b47fa7f52d3f195",
+    "url": "/star/static/media/card1.9d7f5105.svg"
+  },
+  {
+    "revision": "8a7a392a7022fc48dd3e49aad5eff3c5",
+    "url": "/star/static/media/card2.8a7a392a.svg"
+  },
+  {
+    "revision": "e061caf48bab3537b49dd7717f221f2a",
+    "url": "/star/static/media/card3.e061caf4.svg"
+  },
+  {
+    "revision": "26306a76feb49cae0253ce04b66e5ca8",
+    "url": "/star/static/media/hero1.26306a76.jpg"
+  },
+  {
+    "revision": "78f3123c6bd170fff0464868be4d6de0",
+    "url": "/star/static/media/search.78f3123c.svg"
+  }
+]);
